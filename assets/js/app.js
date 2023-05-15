395ebed6bb66199e461e1a979c7e71ba0aa67ba5
Godwin Idemudia <godwinide@gmail.com>
2023-05-15 03:14:51 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+const handleTweet = () => {
+    const tweet = `I just joined the $FAMILY @familycoinindex %0A%0A`;
+    window.open(`https://twitter.com/intent/tweet?text=${tweet}&url=https://familycoin.xyz`, '_blank');
+}
